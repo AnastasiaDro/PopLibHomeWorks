85585001f9e965fc27f108b50b0796fdfa118afc
AnastasiaDro <adrogunova@gmail.com>
2020-08-01 11:31:55 +0300

    - Сделала UserDao интерфейс - доделала все подготовительные классы
    TODO:
    - активити, презентер и кнопки

--- app/src/main/java/com/geekbrains/myapplication/App.java
+++ app/src/main/java/com/geekbrains/myapplication/App.java
@@ -1,8 +1,19 @@
 package com.geekbrains.myapplication;
 
 import android.app.Application;
+import androidx.room.Room;
 
 public class App extends Application {
 
-    private static
+    private static AppDatabase appDatabase;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        appDatabase = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "room_database").build();
+    }
+
+    public static AppDatabase getAppDatabase() {return appDatabase;}
 }
