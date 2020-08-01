a7910bb0c10a355d84651a167df361c277b0b977
AnastasiaDro <adrogunova@gmail.com>
2020-08-01 13:11:32 +0300

    - Подключила ButterKnife - стала делать обработку кнопок TODO: - активити, презентер и кнопки

--- app/src/main/java/com/geekbrains/myapplication/presenter/Presenter.java
+++ app/src/main/java/com/geekbrains/myapplication/presenter/Presenter.java
@@ -1,6 +1,18 @@
 package com.geekbrains.myapplication.presenter;
 
+import com.geekbrains.myapplication.App;
+import com.geekbrains.myapplication.room.UserDao;
+
 public class Presenter {
 
     private static final String TAG = "Presenter";
+
+    private UserDao userDao;
+
+    public Presenter()
+    {userDao = App.getAppDatabase().userDao();}
+
+    public void addUser(String name, String Surname, String age){
+
+    }
 }
--- app/src/main/java/com/geekbrains/myapplication/view/MainActivity.java
+++ app/src/main/java/com/geekbrains/myapplication/view/MainActivity.java
@@ -3,16 +3,24 @@ package com.geekbrains.myapplication.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.geekbrains.myapplication.R;
 import com.geekbrains.myapplication.presenter.Presenter;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import moxy.MvpAppCompatActivity;
 import moxy.presenter.InjectPresenter;
 
 public class MainActivity extends MvpAppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    EditText nameEditText;
+    EditText surnameEditText;
+    EditText ageEditText;
 
     @InjectPresenter
     Presenter presenter;
@@ -21,7 +29,23 @@ public class MainActivity extends MvpAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        init();
     }
 
+    @OnClick({R.id.addBtn})
+    public void onClickAddBtn(View view){
+        Log.d(TAG, "onAddBtn");
 
+        String name = nameEditText.getText().toString();
+        String surname = surnameEditText.getText().toString();
+        String age = ageEditText.getText().toString();
+        presenter.addUser(name, surname, age);
+    }
+
+    public void init() {
+        nameEditText = findViewById(R.id.nameEditText);
+        surnameEditText = findViewById(R.id.surnameEditText);
+        ageEditText = findViewById(R.id.surnameEditText);
+    }
 }
