94b41922d3e0edd4e21484a57aea819654c0d914
AnastasiaDro <adrogunova@gmail.com>
2020-08-06 21:59:56 +0300

    - сделала первую часть с unit-тестом

--- app/src/main/java/com/geekbrains/myapplication/view/MainActivity.java
+++ app/src/main/java/com/geekbrains/myapplication/view/MainActivity.java
@@ -1,14 +1,21 @@
-package com.geekbrains.myapplication;
+package com.geekbrains.myapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.geekbrains.myapplication.R;
+import com.geekbrains.myapplication.firstExc.CalcAmount;
+
 public class MainActivity extends AppCompatActivity {
+
+    CalcAmount calcAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calcAmount = new CalcAmount();
+        calcAmount.calcSum(5);
     }
 }
