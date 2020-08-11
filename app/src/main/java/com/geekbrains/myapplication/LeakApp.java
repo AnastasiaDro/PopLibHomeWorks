47afa5ee44deb3b2fbe5152ba453daa0af857185
AnastasiaDro <adrogunova@gmail.com>
2020-08-11 17:35:43 +0300

    - решила попробовать Stetcho, почему-то не даёт доступ к эмулятору

--- app/src/main/java/com/geekbrains/myapplication/LeakApp.java
+++ app/src/main/java/com/geekbrains/myapplication/LeakApp.java
@@ -2,6 +2,7 @@ package com.geekbrains.myapplication;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 
 public class LeakApp extends Application {
@@ -9,9 +10,11 @@ public class LeakApp extends Application {
     @Override
     public void onCreate(){
         super.onCreate();
+
         if(LeakCanary.isInAnalyzerProcess(this)){
             return;
         }
         LeakCanary.install(this);
+        Stetho.initializeWithDefaults(this);
     }
 }
